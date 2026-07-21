78cdacda1ce687c186b71ffd746cbd53cd1b7b6b
jafetnajera <jafet.najera@gmail.com>
2026-07-21 09:43:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-registerRootComponent(App);
+// Registramos la aplicación en el sistema principal
+AppRegistry.registerComponent('main', () => App);
+
+// Si detecta que estamos en la web, inyecta la app directamente en la página
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('main');
+  AppRegistry.runApplication('main', { rootTag });
+}
